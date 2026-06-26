f8980755a93710a0d10e29d5102950b90edeb0ba
David Ssali <ssalidm@gmail.com>
2026-06-26 14:39:33 +0200

    update Notification Service Dockerfile

--- notification-service/src/test/java/za/co/pixelly/notification/service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/za/co/pixelly/notification/service/NotificationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package za.co.pixelly.order.service;
+package za.co.pixelly.notification.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderServiceApplicationTests {
+class NotificationServiceApplicationTests {
 
     @Test
     void contextLoads() {
